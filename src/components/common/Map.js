eca68d73afdb7065ce3431658086e4433389250e
janecyyu <janecyyu@gmail.com>
2020-08-17 11:04:46 -0700

    add orginaized data

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -1,39 +1,62 @@
 import React, { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import * as capitalsData from '../../testing_data/capitals.json';
+import * as incidentsData from '../../testing_data/incidents data.json';
 import PlotlyMap from './MapByPlotly';
 
 const Map = () => {
   const [viewport, setViewport] = useState({
     latitude: 37.09024,
     longitude: -95.712891,
-    zoom: 2.5,
+    zoom: 3,
     width: '70vw',
     height: '70vh',
   });
+  const [selected, setSelected] = useState(null);
+
   return (
     <div>
       map viz by Mapbox -
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/janecyyu/ckdp4j03i0arg1imkwcu4kxzy"
+        // mapStyle="mapbox://styles/janecyyu/ckdp4j03i0arg1imkwcu4kxzy"
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
       >
-        {capitalsData.states.map(c => (
-          <Marker latitude={parseInt(c.lat)} longitude={parseInt(c.long)}>
-            <div>📍</div>
+        {incidentsData.data.map(incident => (
+          <Marker
+            latitude={parseInt(incident.lat)}
+            longitude={parseInt(incident.lon)}
+          >
+            <button
+              onClick={e => {
+                e.preventDefault();
+                // setSelected([incident.lat, incident.lon, incident.text]);
+                <Popup latitude={incident.lat} longitude={incident.lon}>
+                  <div>{incident.text}</div>
+                </Popup>;
+              }}
+            >
+              📍
+            </button>
           </Marker>
         ))}
+        {/* {selected ? (
+          <Popup latitude={selected[0]} longitude={selected[1]}>
+            <div>{selected[2]}</div>
+          </Popup>
+        ) : null} */}
       </ReactMapGL>
-      map viz by Plotly -
-      <div id="myDiv">
-        <PlotlyMap />
-      </div>
     </div>
   );
 };
 
 export default Map;
+{
+  /* map viz by Plotly -
+      <div id="myDiv">
+        <PlotlyMap />
+      </div> */
+}
