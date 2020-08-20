17b50d8c430c22483e0f4af63906569fcf6ccaa2
andrewhpark25 <andrewhpark25@gmail.com>
2020-08-20 15:35:25 -0700

    added first version of Loading Page

---
+++ src/components/common/Loading.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export const Loading = () => {
+  return (
+    <main>
+      <header>
+        <div className="top">
+          <h1>Find Excessive Use of Force Incidents in Your City </h1>
+          <div>
+            <div>
+              <a className="map" href="#">
+                View Map
+              </a>
+            </div>
+            <p>or type of force</p>
+          </div>
+        </div>
+      </header>
+      <section className="intro">
+        <h2>What is Human Rights Considered?</h2>
+        <div>
+          <p>
+            Human Rights Considered is a public police brutality incident
+            viewing application
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>About Human Rights First</h2>
+        <div>
+          <p>
+            Human Rights First is an independent advocacy and action
+            organization that challenges America to live up to its ideals. We
+            believe American leadership is essential in the global struggle for
+            human rights, so we press the U.S. government and private companies
+            to respect human rights and the rule of law. When they fail, we step
+            in to demand reform, accountability and justice. Around the world,
+            we work where we can best harness American influence to secure core
+            freedoms.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
