087cd8f1f3fdc03762ed005446c8836bea42a0b8
janecyyu <janecyyu@gmail.com>
2020-09-10 16:59:24 -0700

    change input to dropdown

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -162,9 +162,16 @@ const Map = () => {
             <label>
               Search by city name:
               <br />
-              <input type="text" name="cityName" onChange={handleCityChange} />
+              <input type="hidden" name="country" id="countryId" value="US" />
+              <select name="state" class="states order-alpha" id="stateId">
+                <option value="">Select State</option>
+              </select>
+              <select name="city" class="cities order-alpha" id="cityId">
+                <option value="">Select City</option>
+              </select>
+              <br />
+              <input type="submit" value="Submit" onClick={submitCityHandler} />
             </label>
-            <input type="submit" value="Submit" onClick={submitCityHandler} />
           </form>
         </div>
 
