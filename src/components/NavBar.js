540c7fbc0f2174360a67fcb6b776b2bf058b4ec6
jasonelliots <j.elliotschwartz@gmail.com>
2020-10-15 13:35:15 -0700

    updated copy and content ordering

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,11 +10,13 @@ const NavBar = () => {
   return (
     <StyledHeader>
       <div className="logo">
-        <img
-          id="logo"
-          src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/423165_10150686770200747_669805325_n.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=-TaZmzOKohgAX8W3jk4&_nc_ht=scontent-sjc3-1.xx&oh=88b11d2fdc32243402fed9c37667b936&oe=5F8F34C8"
-          alt="human-rights-first-logo"
-        />
+        <a href="https://www.humanrightsfirst.org/">
+          <img
+            id="logo"
+            src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/423165_10150686770200747_669805325_n.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=-TaZmzOKohgAX8W3jk4&_nc_ht=scontent-sjc3-1.xx&oh=88b11d2fdc32243402fed9c37667b936&oe=5F8F34C8"
+            alt="human-rights-first-logo"
+          />
+        </a>
       </div>
       <Menu mode="horizontal">
         <Menu.Item>
