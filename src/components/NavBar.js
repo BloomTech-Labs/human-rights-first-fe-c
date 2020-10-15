52d2733bccb4bbc2fd6dee88a6ada36947757ba5
jasonelliots <j.elliotschwartz@gmail.com>
2020-10-15 11:04:26 -0700

    adds logo in header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
     <Header
       style={{
         display: 'flex',
-        height: '20vh',
+        height: '21vh',
         justifyContent: 'space-between',
         alignItems: 'center',
         backgroundColor: 'white',
@@ -26,7 +26,11 @@ const NavBar = () => {
       }}
     >
       <div className="logo">
-        <span> Test </span>
+        <img
+          id="logo"
+          src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/423165_10150686770200747_669805325_n.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=-TaZmzOKohgAX8W3jk4&_nc_ht=scontent-sjc3-1.xx&oh=88b11d2fdc32243402fed9c37667b936&oe=5F8F34C8"
+          alt="human-rights-first-logo"
+        />
       </div>
       <Menu mode="horizontal">
         <Menu.Item>
