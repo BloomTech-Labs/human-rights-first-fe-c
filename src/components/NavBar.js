b0a9ef2ef097449c5b627020f525acd53f8550f7
jasonelliots <j.elliotschwartz@gmail.com>
2020-10-22 10:11:54 -0700

    fixed nav + map margins

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './../styles/index.css';
 import 'antd/dist/antd.css';
 import logo from '../assets/logo.png';
-import { Menu, Typography } from 'antd';
+import { Menu } from 'antd';
 
 // style component imports
+
 import {
   StyledHeader,
   StyledTitle,
