9f0046ef5a699d8173406ac3b3426be3a54704a8
Maria Trujillo <mptruj015@gmail.com>
2020-11-13 20:23:46 +0100

    added pie chart component

--- src/components/DashboardChart.js
+++ src/components/DashboardChart.js
@@ -1,29 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import * as am4core from '@amcharts/amcharts4/core';
-import * as am4charts from '@amcharts/amcharts4/charts';
-import am4themes_animated from '@amcharts/amcharts4/themes/animated';
-import axios from 'axios';
-
+import PieChart from './PieChart';
 const DashboardChart = () => {
-  const [incidents, setIncidents] = useState([]);
-
-  useEffect(() => {
-    // const getIncidents = () => {
-    //   setIncidents(resposnse);
-    // };
-    // getIncidents();
-  }, []);
-
-  // let dash_chart = am4core.create('chartdiv', am4charts.XYChart);
-
   return (
-    // <div id="chartdiv" style={{ width: '100%', height: '500px' }}>
-    //   {dash_chart} <p>hello</p>
-    // </div>
-    <div>
-      <h1>hello</h1>
+    <div className="dash-chart-container">
+      <PieChart />
     </div>
   );
 };
-
 export default DashboardChart;
