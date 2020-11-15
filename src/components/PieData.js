da2dbaf67f22ef56c7eea5d48eeabc583a2fdfa5
Maria Trujillo <mptruj015@gmail.com>
2020-11-15 16:19:18 +0100

    added function that refactors query hook data into array of objs

--- src/components/PieData.js
+++ src/components/PieData.js
@@ -1,11 +1,23 @@
 import { useIncidents } from '../state/query_hooks/useIncidents';
-import { useIncident } from '../state/query_hooks/useIncident';
 import { useCategoryCount } from '../helpers/useCategoryCount';
 
 export const PieData = () => {
   const incidentsQuery = useIncidents();
 
-  let data = useCategoryCount(incidentsQuery.data);
+  let raw_data = useCategoryCount(incidentsQuery.data);
 
-  return data;
+  let raw_data_length = Object.keys(raw_data).length;
+  let keys = Object.keys(raw_data);
+  let values = Object.values(raw_data);
+
+  let data_container = [];
+  let data_obj = {};
+
+  for (let i = 0; i < raw_data_length; i++) {
+    data_obj[i] = { type_of_force: keys[i], count: values[i] };
+  }
+
+  data_container.push(data_obj);
+
+  return data_container;
 };
