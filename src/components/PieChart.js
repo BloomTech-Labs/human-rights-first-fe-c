b8ba81a69748d2340f4fb75b59dc7dc5bad7f999
Maria Trujillo <mptruj015@gmail.com>
2020-11-17 18:36:20 +0100

    added legend for pie chart

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -16,10 +16,27 @@ const PieChart = data => {
     pieSeries.dataFields.value = 'count';
     pieSeries.dataFields.category = 'type_of_force';
     chart.current = pie_chart;
+
+    // set legend
+    pie_chart.legend = new am4charts.Legend();
+
+    // configure legend
+    pie_chart.legend.labels.template.textDecoration = 'none';
+    pie_chart.legend.valueLabels.template.textDecoration = 'none';
+
+    var as = pie_chart.legend.labels.template.states.getKey('active');
+    as.properties.textDecoration = 'line-through';
+    as.properties.fill = am4core.color('#000');
+
+    var as2 = pie_chart.legend.valueLabels.template.states.getKey('active');
+    as2.properties.textDecoration = 'line-through';
+    as2.properties.fill = am4core.color('#000');
+
     return () => {
       pie_chart.dispose();
     };
   }, []);
+
   return <div id="chartdiv" style={{ width: '100%', height: '500px' }}></div>;
 };
 export default PieChart;
